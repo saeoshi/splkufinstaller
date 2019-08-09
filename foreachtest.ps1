5a82453ab0a41c8971edf697cd6c65c106961336
Satoshi Udagawa <saeoshi>
2019-08-09 21:42:42 +0900

    add echo statement

--- foreachtest.ps1
+++ foreachtest.ps1
@@ -1,4 +1,5 @@
 foreach ($servlist in Get-Content server.txt) {
     $sess = New-PSSession -ComputerName $servlist -Credential Administrator
     Invoke-Command $sess -FilePath C:\Users\Administrator\Documents\splkinstall.ps1
+    echo  "finish to install :" $servlist
     }
