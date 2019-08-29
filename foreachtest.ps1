c4ec45c01275e21422427a7d0013327c597d3207
Satoshi Udagawa <eswat.dunan@gmail.com>
2019-08-29 13:14:31 +0900

    add file path each script

--- foreachtest.ps1
+++ foreachtest.ps1
@@ -1,4 +1,4 @@
-foreach ($servlist in Get-Content server.txt) {
+foreach ($servlist in Get-Content C:\Users\Administrator\Documents\server.txt) {
     $sess = New-PSSession -ComputerName $servlist -Credential Administrator
     Invoke-Command $sess -FilePath C:\Users\Administrator\Documents\splkinstall.ps1
     echo  "finish to install :" $servlist
